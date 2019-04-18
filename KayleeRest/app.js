28a2b95cbbf8dd083ac9163c6c6ccdff186247f6
gaspare bastone <gaspareb@yahoo.com>
2019-04-18 10:39:08 -0400

    Reset Button

--- KayleeRest/app.js
+++ KayleeRest/app.js
@@ -3,13 +3,9 @@ const debug = require('debug');
 const express = require('express');
 const path = require('path');
 const vehicles = require('./routes/vehicles');
-const vehiclesMongo = require('./routes/vehiclesMongo');
 const reports = require('./routes/reports');
-const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const app = express();
-const kayleeDBUrlPath = 'mongodb://localhost:27017';
-const kayleeDBName = 'kaylee';
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/reports', reports);
 app.use('/vehicles', vehicles);
-app.use('/vehiclesMongo', vehiclesMongo);
 
 app.set('port', process.env.PORT || 3000);
 
